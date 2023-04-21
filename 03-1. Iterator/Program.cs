0f3b8da18a6d88114f9c969d1587b1cc6f0a48a5
수인 강 <ks00in213@gmail.com>
2023-04-21 20:11:50 +0900

    feat : 04. Stack 05. Queue

---
+++ 03-1. Iterator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _03_1._Iterator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ 05. Queue/Queue Adapter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05._Queue
+{
+    /******************************************************
+	 * 어댑터 패턴 (Adapter)
+	 * 
+	 * 한 클래스의 인터페이스를 사용하고자 하는 다른 인터페이스로 변환
+	 ******************************************************/
+
+    public class AdapterQueue<T>
+    {
+        private LinkedList<T> container;
+
+        public AdapterQueue()
+        {
+            container = new LinkedList<T>();
+        }
+
+        public void Enqueue(T item)
+        {
+            container.AddLast(item);
+        }
+
+        public T Dequeue()
+        {
+            T value = container.First<T>();
+            container.RemoveFirst();
+            return value;
+        }
+
+        public T Peek()
+        {
+            return container.First<T>();
+        }
+    }
+}
