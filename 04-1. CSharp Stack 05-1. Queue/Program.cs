a96975e54f5d597db13c9cd2f5b17682cbaa1bf7
수인 강 <ks00in213@gmail.com>
2023-04-21 20:34:49 +0900

    feat : 수정

--- 04-1. CSharp Stack 05-1. Queue/Program.cs
+++ 04-1. CSharp Stack 05-1. Queue/Program.cs
@@ -2,19 +2,70 @@
 {
     internal class Program
     {
-    // 1. 스택(어댑터), 큐(순환배열) 구현
-    // + 주석 꼭 달아줘
-    // =========================
-    // 2. 스택
-    // 괄호 검사기 ( ) ( ), ( ( ) ) | ((), )()(
-    // 스택 계산기 1+3*7-6
-    // 3. 큐 
-    // 속도가 빠른 플레이어부터 행동
-    // 요세푸스 순열
+        // 1. 스택(어댑터), 큐(순환배열) 구현
+        // + 주석 꼭 달아줘
+
+        public class AdapterStack<T>
+        {
+            private List<T> container;
+
+            public AdapterStack()
+            {
+                container = new List<T>();
+            }
+
+            public void Push(T item)
+            {
+                container.Add(item);
+            }
+
+            public T Pop()
+            {
+                T item = container[container.Count - 1];
+                container.RemoveAt(container.Count - 1);
+                return item;
+            }
+
+            public T Peek()
+            {
+                return container[container.Count - 1];
+            }
+        }
+
+
+
+
+
+        // =========================
+
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // 2.스택
+            // 괄호 검사기 ( ) ( ), ( ( ) ) | ((), )()(
+            // 스택 계산기 1+3*7-6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // 3. 큐 
+            // 속도가 빠른 플레이어부터 행동
+            // 요세푸스 순열
         }
     }
 }
