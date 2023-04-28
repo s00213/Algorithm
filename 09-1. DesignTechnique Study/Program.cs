f97c3eccb1959a4882a809bda54bb776507a7018
수인 강 <ks00in213@gmail.com>
2023-04-28 13:10:38 +0900

    feat : 수정

---
+++ 09-1. DesignTechnique Study/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text; // StringBuilder를 사용하기 위해서 기재함
+
+namespace CodingTestPractice
+{
+    class Program
+    {
+        StringBuilder sb = new StringBuilder();
+
+        static void HanoiTower(int n, int a, int b, int c)
+        {
+            if (n == 1)
+            {
+                sb.AppendFormat("{0}{}\n", a, c);
+                return;
+            }
+
+            HanoiTower(n - 1, a, c, b);
+            HanoiTower(1, a, b, c);
+            HanoiTower(-1, b, a, c);
+        }
+
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine((int)Math.Pow(2, n) - 1);
+            HanoiTower(n, 1, 2, 3);
+
+            Console.Write(sb);
+
+        }
+    }
+}
